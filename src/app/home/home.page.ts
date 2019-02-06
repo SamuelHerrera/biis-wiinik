62c21e1490c819064693c827674d65addeaea17c
Jesus Samuel Herrera Fuente <samuel.herrera@4thsource.com>
2019-02-06 08:36:35 -0600

    working map

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,118 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Geolocation, Geoposition } from '@ionic-native/geolocation/ngx';
+import { NavController, MenuController } from '@ionic/angular';
+
+declare var google;
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+
+  map: any;
+  directionsService: any = null;
+  directionsDisplay: any = null;
+  bounds: any = null;
+  myLatLng: any;
+  waypoints: any[];
+
+  constructor(public navCtrl: NavController, public geolocation: Geolocation, private menu: MenuController) {
+
+    this.directionsService = new google.maps.DirectionsService();
+    this.directionsDisplay = new google.maps.DirectionsRenderer();
+    this.bounds = new google.maps.LatLngBounds();
+
+    this.waypoints = [
+      {
+        location: { lat: 20.9920862, lng: -89.7200625 },
+        stopover: true,
+      },
+      {
+        location: { lat: 20.9920462, lng: -89.7200325 },
+        stopover: true,
+      }
+    ];
+  }
+
+  ngOnInit(): void {
+    this.getPosition();
+  }
+
+
+  getPosition(): any {
+    this.geolocation.getCurrentPosition()
+      .then(response => {
+        this.loadMap(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  loadMap(position: Geoposition) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    // create a new map by passing HTMLElement
+    const mapEle: HTMLElement = document.getElementById('map');
+
+    // create LatLng object
+    this.myLatLng = { lat: latitude, lng: longitude };
+
+    // create map
+    this.map = new google.maps.Map(mapEle, {
+      center: this.myLatLng,
+      zoom: 14,
+      disableDefaultUI: true
+    });
+
+    this.directionsDisplay.setMap(this.map);
+
+    google.maps.event.addListenerOnce(this.map, 'idle', () => {
+      const marker = new google.maps.Marker({
+        position: this.myLatLng,
+        map: this.map,
+        title: 'Mi posicion',
+        animation: 'BOUNCE'
+      });
+      mapEle.classList.add('show-map');
+      // this.calculateRoute();
+    });
+  }
+
+  private calculateRoute() {
+
+    this.bounds.extend(this.myLatLng);
+
+    this.waypoints.forEach(waypoint => {
+      const point = new google.maps.LatLng(waypoint.location.lat, waypoint.location.lng);
+      this.bounds.extend(point);
+    });
+
+    this.map.fitBounds(this.bounds);
+
+    this.directionsService.route({
+      origin: new google.maps.LatLng(this.myLatLng.lat, this.myLatLng.lng),
+      destination: new google.maps.LatLng(this.myLatLng.lat, this.myLatLng.lng),
+      waypoints: this.waypoints,
+      optimizeWaypoints: true,
+      travelMode: google.maps.TravelMode.DRIVING,
+      avoidTolls: true
+    }, (response, status) => {
+      if (status === google.maps.DirectionsStatus.OK) {
+        console.log(response);
+        this.directionsDisplay.setDirections(response);
+      } else {
+        alert('Could not display directions due to: ' + status);
+      }
+    });
+
+  }
+
+  public openFirst() {
+    this.menu.enable(true, 'first');
+    this.menu.open('first');
+  }
 
 }
